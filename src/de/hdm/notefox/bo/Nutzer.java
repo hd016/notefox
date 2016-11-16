db83b14e9a6a8e81c653294eace9b02eb326319a
Muhammed Simsek <Muhammed Simsek@MuhammedSimsek>
2016-11-16 16:38:35 +0100

    Nutzer-Klasse erstellt

---
+++ src/de/hdm/notefox/bo/Nutzer.java
@@ -0,0 +1,40 @@
+package de.hdm.notefox.bo;
+
+public class Nutzer {
+
+  /**
+   * Variablen des Nutzers.
+  */
+
+private int nutzerid;
+private String name = "";
+private String passwort = "";
+  
+/**Auslesen und Setzen der Variablen
+ */
+  public int getNutzerId() {
+    return this.nutzerid;
+  }
+
+  public void setNutzerId(int nutzerid) {
+    this.nutzerid = nutzerid;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public String getPasswort() {
+    return this.passwort;
+  }
+
+  public void setPasswort(String passwort) {
+    this.passwort = passwort;
+  }
+
+
+}
