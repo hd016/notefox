391a0899cf8b28a140be4f01f561145815618e30
Muhammed Simsek <Muhammed Simsek@MuhammedSimsek>
2016-11-16 16:30:40 +0100

    Business Objekt Datum erstellt

---
+++ src/de/hdm/notefox/bo/Datum.java
@@ -0,0 +1,41 @@
+package de.hdm.notefox.bo;
+
+import java.util.Date;
+
+public class Datum {
+	
+	/**Variablen der Klasse Datum.
+	 */
+
+	private int faelligkeitId;
+	
+	private boolean status;
+	
+	private Date faelligkeitsdatum;
+	
+/**Auslesen und Setzen der Variablen.
+ */
+	public int getFaelligkeitId() {
+		return faelligkeitId;
+	}
+
+	public void setFaelligkeitId(int faelligkeitId) {
+		this.faelligkeitId = faelligkeitId;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public Date getFaelligkeitsdatum() {
+		return faelligkeitsdatum;
+	}
+
+	public void setFaelligkeitsdatum(Date faelligkeitsdatum) {
+		this.faelligkeitsdatum = faelligkeitsdatum;
+	} 
+}
