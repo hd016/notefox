2feda76d563647e89466ca252f98fc989425d6ac
Muhammed Simsek <Muhammed Simsek@MuhammedSimsek>
2016-11-16 16:43:24 +0100

    Business Objekt Notiz erstellt

---
+++ src/de/hdm/notefox/bo/Notiz.java
@@ -0,0 +1,28 @@
+package de.hdm.notefox.bo;
+
+public class Notiz extends Notizobjekt {
+
+	/** Unique IDentifier
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Notizquelle notizquelle;
+	private Datum datum;
+	
+
+	  /** Variablen der Notiz
+	   */
+	private int notizId;
+
+	  /** Auslesen und Setzen der Variablen
+	   */
+	public int getNotizId() {
+		return notizId;
+	}
+
+
+	public void setNotizId(int notizId) {
+		this.notizId = notizId;
+	}
+	
+}
