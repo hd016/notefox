7f2998e5b668a5716f7cb77629cc85ae9897941c
Muhammed Simsek <Muhammed Simsek@192.168.1.89>
2016-11-23 15:06:54 +0100

    notiz und noitbuch reports erstellt

---
+++ src/de/hdm/notefox/shared/report/AlleNotizbuecherDesNutzersReport.java
@@ -0,0 +1,14 @@
+package de.hdm.notefox.shared.report;
+
+/**
+ * Alle Notizbücher des Nutzers werden mit diesem Report dargestellt.
+ */
+public class AlleNotizbuecherDesNutzersReport
+	extends SimpleReport {
+
+	/** 
+	 * Unique IDentifier
+	 */
+  private static final long serialVersionUID = 1L;
+
+}
