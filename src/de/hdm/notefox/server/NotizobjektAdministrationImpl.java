51132b8c07512ba7ea0f56845486154c20dc1b4c
Esra <Esra@Esra-VAIO>
2016-11-27 19:19:17 +0100

    NotizobjektAdministrationImpl-Klasse erstellt

---
+++ src/de/hdm/notefox/server/NotizobjektAdministrationImpl.java
@@ -0,0 +1,12 @@
+package de.hdm.notefox.server;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+import de.hdm.notefox.server.db.*;
+import de.hdm.notefox.server.shared.*;
+import de.hdm.notefox.server.shared.bo.*;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+
