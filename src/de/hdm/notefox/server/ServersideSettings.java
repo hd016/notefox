1464d0488ad1d1ca71312e6266f2f1f8be6978ee
Esra <Esra@Esra-VAIO>
2016-11-27 20:22:24 +0100

    Realisierung des Loggers und weitere Anpassungen- Erweiterung in ServersideSettings

--- src/de/hdm/notefox/server/ServersideSettings.java
+++ src/de/hdm/notefox/server/ServersideSettings.java
@@ -26,3 +26,40 @@ public class ServersideSettings extends CommonSettings {
   private static final Logger log = Logger.getLogger(LOGGER_NAME);
 
   
+/**
+ * <p>
+ * Auslesen des applikationsweiten (Server-seitig!) zentralen Loggers.
+ * </p>
+ * 
+ * <h2>Anwendungsbeispiel:</h2> Zugriff auf den Logger herstellen durch:
+ * 
+ * <pre>
+ * Logger logger = ServerSideSettings.getLogger();
+ * </pre>
+ * 
+
+ * 
+ * <h2>HINWEIS:</h2>
+ * <p>
+ * Beachten Sie, dass Sie den auszugebenden Log nun nicht mehr durch
+ * bedarfsweise Einfügen und Auskommentieren etwa von
+ * <code>System.out.println(...);</code> steuern. Sie belassen künftig
+ * sämtliches Logging im Code und können ohne abermaliges Kompilieren den Log
+ * Level "von außen" durch die Datei <code>logging.properties</code> steuern.
+ * Sie finden diese Datei in dem <code>war/WEB-INF</code>-Ordner Ihres
+ * Projekts. Der dort standardmäßig vorgegebene Log Level ist
+ * <code>WARN</code>. Dies würde bedeuten, dass Sie keine <code>INFO</code>
+ * -Meldungen wohl aber <code>WARN</code>- und <code>SEVERE</code>-Meldungen
+ * erhielten. Wenn Sie also auch Log des Levels <code>INFO</code> wollten,
+ * müssten Sie in dieser Datei <code>.level = INFO</code> setzen.
+ * </p>
+ * 
+ * Weitere Infos siehe Dokumentation zu Java Logging.
+ * 
+ * @return die Logger-Instanz für die Server-Seite
+ */
+public static Logger getLogger() {
+  return log;
+}
+
+}
