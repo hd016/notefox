1027095e7ae0a6de9c78e3b360b803d5ceaa0f89
Harun Dalici <hd016@hdm-stuttgart.de>
2016-11-29 17:55:40 +0100

    notefox.css/html & update notefox.java -> vPanel & cellTree in haupt div

--- src/de/hdm/notefox/client/Notefox.java
+++ src/de/hdm/notefox/client/Notefox.java
@@ -4,36 +4,84 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gwt.cell.client.AbstractCell;
-import com.google.gwt.cell.client.Cell;
-import com.google.gwt.cell.client.TextCell;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.cellview.client.CellTree;
-import com.google.gwt.user.cellview.client.
-HasKeyboardSelectionPolicy.KeyboardSelectionPolicy;
-import com.google.gwt.user.cellview.client.TreeNode;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.view.client.ListDataProvider;
-import com.google.gwt.view.client.SingleSelectionModel;
 import com.google.gwt.view.client.TreeViewModel;
+
+import de.hdm.notefox.client.gui.NotizBaumModel;
+//import de.hdm.notefox.shared.LoginInfo;
+//import de.hdm.notefox.shared.LoginServiceAsync;
+import de.hdm.notefox.shared.NotizobjektAdministrationAsync;
+import de.hdm.notefox.shared.Nutzer;
+import de.hdm.notefox.shared.bo.Notiz;
+import de.hdm.notefox.shared.bo.Notizbuch;
 
 public class Notefox implements EntryPoint {
 
 	
-	Button Test = new Button("Mein Profil");
+	HorizontalPanel hPanel = new HorizontalPanel();
+	Button loeschen = new Button("Löschen");
+	Button editieren = new Button("Editieren");
+	Button suchen = new Button("Suchen");
+	Button freigeben = new Button("Freigeben");
+	Label test = new Label("Klick erfolgreich!");
+	
 	VerticalPanel vPanel = new VerticalPanel();
+	Button NotizBuch = new Button("Notizbuch");
+	Button Nutzer = new Button("Nutzer");
+	Button Profil = new Button("Profil");
+
+	CellTree celltree = new CellTree(new NotizBaumModel(), null);
+
 
 	
+	
+	@Override
 	public void onModuleLoad() {
-
-		Test.addStyleName("gwt-Green-Button");
-		vPanel.add(Test);
-		RootPanel.get("gwtContainer").add(vPanel);
+		
+			vPanel.add(NotizBuch);
+			vPanel.add(Nutzer);
+			vPanel.add(Profil);
+			NotizBuch.addStyleName("gwt-Green-Button");
+			Nutzer.addStyleName("gwt-Green-Button");
+			Profil.addStyleName("gwt-Green-Button");
+			
+			// landscape1Btn.addClickHandler(new LS1ClickHandler());
+			
+			NotizBuch.addClickHandler(new CellTreeClickHandler());
+			
+			RootPanel.get("gwtContainer").add(vPanel);
+			
+			
+		
 	}
+	private class CellTreeClickHandler implements ClickHandler{
 
+		@Override
+		public void onClick(ClickEvent event) {
+			RootPanel.get("text").add(test);
+			RootPanel.get("text").add(celltree);
+			
+			
+		}
 
- 
+	
+	
+		
+	}
+	
+	
 }
+	
