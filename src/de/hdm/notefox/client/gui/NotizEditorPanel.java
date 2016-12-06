41d400ff2f5ca5be164a1b4c0515f0d9a722c46d
Harun Dalici <hd016@hdm-stuttgart.de>
2016-12-06 15:05:40 +0100

    erstellen und verschieben von NotizEditorPanel

--- src/de/hdm/notefox/client/gui/NotizEditorPanel.java
+++ src/de/hdm/notefox/client/gui/NotizEditorPanel.java
@@ -1,16 +1,18 @@
-package de.hdm.notefox.client;
+package de.hdm.notefox.client.gui;
 
 import com.google.gwt.core.shared.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RichTextArea;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-import de.hdm.notefox.client.gui.RichTextToolbar;
 import de.hdm.notefox.shared.NotizobjektAdministration;
 import de.hdm.notefox.shared.NotizobjektAdministrationAsync;
 import de.hdm.notefox.shared.bo.Notiz;
@@ -23,21 +25,30 @@ public class NotizEditorPanel extends VerticalPanel {
 	RichTextArea area = new RichTextArea();
 	RichTextToolbar Rich = new RichTextToolbar(area);
 	TextBox titel = new TextBox();
-
+	DialogBox bx = new DialogBox();
+	
+	
 	public NotizEditorPanel(final Notiz notiz) {
 		add(titel);
 		add(Rich);
 		add(area);
 		add(speichern);
-
+		SafeHtmlBuilder html = new SafeHtmlBuilder();
+		
+		area.addStyleName("textarea");
+		speichern.addStyleName("gwt-Green-Button");
+		
+		
 		speichern.addClickHandler(new ClickHandler() {
 
 			@Override
 			public void onClick(ClickEvent event) {
-				notiz.setInhalt(area.getHTML());
-				notiz.setTitel(titel.getText());
-
-				administration.speichern(notiz, new AsyncCallback<Void>() {
+			notiz.setInhalt(area.getHTML());
+			notiz.setTitel(titel.getText());
+			Window.alert("Titel: " + titel.getText() + "\n Inhalt: " + area.getHTML());
+			
+			
+			/*	administration.speichern(notiz, new AsyncCallback<Void>() {
 
 					@Override
 					public void onFailure(Throwable caught) {
@@ -54,6 +65,9 @@ public class NotizEditorPanel extends VerticalPanel {
 				});
 			}
 		});
+		*/
 	}
-
-}
+		
+});
+		
+	}}
