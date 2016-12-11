1dae372dd4e0aecc4141114673cefa25ea05a847
Harun Dalici <hd016@hdm-stuttgart.de>
2016-12-11 16:36:10 +0100

    loginklassen erstellt

---
+++ src/de/hdm/notefox/server/LoginServiceImpl.java
@@ -0,0 +1,58 @@
+package de.hdm.notefox.server;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+import de.hdm.notefox.server.db.NutzerMapper;
+import de.hdm.notefox.shared.LoginInfo;
+import de.hdm.notefox.shared.LoginService;
+import de.hdm.notefox.shared.Nutzer;
+
+
+/**
+ * TODO
+ * @author Harun Dalici & Muhammed Simsek
+ *
+ */
+public class LoginServiceImpl extends RemoteServiceServlet implements LoginService {
+
+	private static final long serialVersionUID = 1L;
+
+	private static LoginService loginService;
+
+	public static LoginService loginService() {
+		if (loginService == null) {
+			loginService = new LoginServiceImpl();
+		}
+		return loginService;
+	}
+
+	@Override
+	public LoginInfo login(String requestUri) {
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		LoginInfo loginInfo = new LoginInfo();
+
+		if (user != null) {
+			loginInfo.setLoggedIn(true);
+			loginInfo.setEmail(user.getEmail());
+			loginInfo.setNutzer(NutzerMapper.nutzerMapper().findByEmail(user.getEmail()));
+			loginInfo.setLogoutUrl(userService.createLogoutURL(requestUri));
+		} else {
+			loginInfo.setLoggedIn(false);
+			loginInfo.setLoginUrl(userService.createLoginURL(requestUri));
+		}
+		return loginInfo;
+	}
+
+	public Nutzer getCurrentNutzer() {
+		LoginInfo login = login("");
+		if (login.isLoggedIn()) {
+			return login.getNutzer();
+		} else {
+			return null;
+		}
+	}
+}
---
+++ src/de/hdm/notefox/shared/LoginService.java
@@ -0,0 +1,34 @@
+package de.hdm.notefox.shared;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+import de.hdm.notefox.shared.Nutzer;
+
+/**
+ * Synchrone Schnittstelle für die Klasse <code>LoginServiceImpl</code>.
+ * 
+ * 
+ * @author Harun Dalici & Muhammed Simsek
+ */
+
+@RemoteServiceRelativePath("login")
+public interface LoginService extends RemoteService {
+
+	/**
+	 * Ausführen des Logins und ablegen aller relevanten Nutzer Informationen in
+	 * einem LoginInfo Objekt
+	 * 
+	 * @param requestUri
+	 *            Die Basis URL der aufrufenden Seite (ermittelt über
+	 *            GWT.getHostPageBaseURL())
+	 * @return
+	 */
+	public LoginInfo login(String requestUri);
+
+	/**
+	 * Auslesen des aktuellen Profils
+	 */
+	Nutzer getCurrentNutzer();
+
+}
