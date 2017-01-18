c986f9ff078bd4cec079b50abae3a973a6f90491
Esra <es061@hdm-stuttgart.de>
2017-01-19 00:21:25 +0100

    Anpassung und Hinzufügen der Kommentare

--- src/de/hdm/notefox/shared/report/ReportWriter.java
+++ src/de/hdm/notefox/shared/report/ReportWriter.java
@@ -1,33 +1,28 @@
 package de.hdm.notefox.shared.report;
 
 /**
- * Wird benötigt, um vom Server zur Verfügung gestellten Report auf dem Client
+ * Wird benÃ¶tigt, um vom Server zur VerfÃ¼gung gestellten Report auf dem Client
  * ein menschenlesbares Format zu verwandeln.
  */
 public abstract class ReportWriter {
 
-	
-  /**
-   * Das AlleNotizbuecherDesNutzersReport wird in das
-   * Zielformat übersetzt.
-   */
-  public abstract void process(AlleNotizbuecherDesNutzersReport r);
+	/**
+	 * Das AlleNotizbuecherDesNutzersReport wird in das Zielformat Ã¼bersetzt.
+	 */
+	public abstract void process(AlleNotizbuecherDesNutzersReport r);
 
-  /**
-   * Das AlleNotizenDesNutzersReport wird in das
-   * Zielformat übersetzt.
-   */
-  public abstract void process(AlleNotizenDesNutzersReport r);
-  
-  /**
-   * Das AlleNotizbuecherAllerNutzerReport wird in das
-   * Zielformat übersetzt.
-   */
-  public abstract void process(AlleNotizbuecherAllerNutzerReport r);
+	/**
+	 * Das AlleNotizenDesNutzersReport wird in das Zielformat Ã¼bersetzt.
+	 */
+	public abstract void process(AlleNotizenDesNutzersReport r);
 
-  /**
-   * Das AlleNotizenAllerNutzerReport wird in das
-   * Zielformat übersetzt.
-   */
-  public abstract void process(AlleNotizenAllerNutzerReport r);
+	/**
+	 * Das AlleNotizbuecherAllerNutzerReport wird in das Zielformat bersetzt.
+	 */
+	public abstract void process(AlleNotizbuecherAllerNutzerReport r);
+
+	/**
+	 * Das AlleNotizenAllerNutzerReport wird in das Zielformat ï¿½bersetzt.
+	 */
+	public abstract void process(AlleNotizenAllerNutzerReport r);
 }
