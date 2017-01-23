b15b6573c4bebc3ca71b32a85cf8dd166605fe59
Neri <nk058@hdm-stuttgart.de>
2017-01-23 01:18:43 +0100

    Kommentar für die Klasse NotizquelleDialogbox definiert

--- src/de/hdm/notefox/client/gui/NotizquelleDialogBox.java
+++ src/de/hdm/notefox/client/gui/NotizquelleDialogBox.java
@@ -10,43 +10,64 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
+/**
+ * Die Klasse NotizquelleDialogbox, stellt einen Dialogbox zur Verfügung. Davon kann
+ * der Nutzer, den Javascript Code für die Einbettung der Notizquellenübernahme
+ * auf seiner eigenen Website, übermitteln
+ * 
+ * @author Neriman Kocak und Harun Dalici
+ */
+
 public class NotizquelleDialogBox extends DialogBox {
 
 	public NotizquelleDialogBox() {
 
-	
-
 		setText("NoteFox auf deiner Webseite einbetten..");
 		setAnimationEnabled(true);
 		setGlassEnabled(true);
-		//center();
+
 		addStyleName("gwt-DialogBox-Notizquelle");
 		VerticalPanel panel = new VerticalPanel();
 		HorizontalPanel hPanelTextBox = new HorizontalPanel();
 		HorizontalPanel hPanel = new HorizontalPanel();
 
-		
 		Label einleitung = new Label("Bitte Kopiere das HTML Code und füge es auf deiner Webseite ein.");
 
 		TextBox output = new TextBox();
 		output.setReadOnly(isGlassEnabled());
 		output.addStyleName("gwt-TextBox-Dialogbox");
-		output.setText("<a id=\"note-fox-link\" href=\"#\">Link</a><script>var notefoxUrl = \""+GWT.getHostPageBaseURL()+"\";document.getElementById(\"note-fox-link\").setAttribute(\"href\", notefoxUrl + \"?url=\" + window.location);</script>");
-		output.setText("<p><strong>Teile diesen Inhalt auf Notefox!</strong></p><p>&nbsp;</p><p>&nbsp;</p><a id=\"note-fox-link\" href=\"#\"><img src=\"http://leqsico.de/notefox/notefox+.png\" alt=\"\" width=\"500\" height=\"300\" /></a><script>var notefoxUrl =" + GWT.getHostPageBaseURL() +";document.getElementById(\"note-fox-link\").setAttribute(\"href\", notefoxUrl + \"?url=\" + window.location);</script>");
+
+		/* 
+		 * Javascript Code für die Einbettung der Notefox+ Button
+		 */
+		
+		output.setText("<a id=\"note-fox-link\" href=\"#\">Link</a><script>var notefoxUrl = \""
+				+ GWT.getHostPageBaseURL()
+				+ "\";document.getElementById(\"note-fox-link\").setAttribute(\"href\", notefoxUrl + \"?url=\" + window.location);</script>");
+		output.setText(
+				"<p><strong>Teile diesen Inhalt auf Notefox!</strong></p><p>&nbsp;</p><p>&nbsp;</p><a id=\"note-fox-link\" href=\"#\"><img src=\"http://leqsico.de/notefox/notefox+.png\" alt=\"\" width=\"500\" height=\"300\" /></a><script>var notefoxUrl ="
+						+ GWT.getHostPageBaseURL()
+						+ ";document.getElementById(\"note-fox-link\").setAttribute(\"href\", notefoxUrl + \"?url=\" + window.location);</script>");
 		hPanel.add(einleitung);
 		hPanelTextBox.addStyleName("gwt-DialogBox");
 		hPanelTextBox.add(output);
 
-		// main panel = panel
+		/* 
+		 * main panel = panel
+		 */
+		
 		panel.add(hPanel);
 		panel.add(hPanelTextBox);
 		panel.setSpacing(10);
-		// panel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
 
 		setWidget(panel);
 
 	}
 
+	/**
+	 * Die Methode beschreibt das Schließen des Dialogboxes
+	 */
+	
 	protected void onPreviewNativeEvent(NativePreviewEvent event) {
 		super.onPreviewNativeEvent(event);
 		switch (event.getTypeInt()) {
